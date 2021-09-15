9d29b704a211bf01b8ac6c85646187e1b28d876a
rahul <rahulrai@deloitte.com>
2021-09-15 21:46:34 +0530

    adding missing files

--- AKSExample/Controllers/WeatherForecastController.cs
+++ AKSExample/Controllers/WeatherForecastController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace AKSExample.Controllers
@@ -17,25 +19,31 @@ namespace AKSExample.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
-            //var rng = new Random();
-            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            //{
-            //    Date = DateTime.Now.AddDays(index),
-            //    TemperatureC = rng.Next(-20, 55),
-            //    Summary = Summaries[rng.Next(Summaries.Length)]
-            //})
-            //.ToArray();
-            var _result = new StoreContext().Employees.ToList();
-            return Ok(_result);
+            var serviceBaseURI = _configuration.GetValue<string>("service2baseuri");
+            var _client = new HttpClient();
+            var response = await _client.GetAsync($"{serviceBaseURI}");
+            if (response.IsSuccessStatusCode)
+            {
+                var responseData = await response.Content.ReadAsStringAsync();
+                if (responseData.ToString() == "true")
+                {
+                    var _result = new StoreContext().Employees.ToList();
+                    return Ok(_result);
+                }
+                return BadRequest("Response string doesn't match");
+            }
+            return BadRequest("Response from service2 failed");
         }
     }
 }
