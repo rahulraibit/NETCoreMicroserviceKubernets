f2f828d28bb3753f2b492b886f53569fa31b717c
rahul <rahulrai@deloitte.com>
2021-09-16 14:34:50 +0530

    working AKS azure grid messaging

--- AKSExample/Controllers/WeatherForecastController.cs
+++ AKSExample/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 
 namespace AKSExample.Controllers
 {
@@ -44,6 +46,28 @@ namespace AKSExample.Controllers
                 return BadRequest("Response string doesn't match");
             }
             return BadRequest("Response from service2 failed");
+        }
+
+        [HttpPost]
+        public async Task SendEvent([FromQuery] int id)
+        {
+            var client = new RestClient("https://akssubscription.westus2-1.eventgrid.azure.net/api/events");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("aeg-sas-key", "9kko9KY+GXxEn38W5pSKv0iH7DGLioqmZeVeqYnS4aA=");
+            request.AddHeader("Content-Type", "application/json");
+            dynamic body = new System.Dynamic.ExpandoObject();
+            body.Id = Guid.NewGuid().ToString();
+            body.eventType = "EmployeeInserted";
+            body.subject = "New Employee Added";
+            body.eventTime = DateTime.Now.ToString();
+            body.dataversion = "1.0";
+            body.data = new System.Dynamic.ExpandoObject();
+            var data = new ArrayList();
+            data.Add(body);
+            body = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
         }
     }
 }
